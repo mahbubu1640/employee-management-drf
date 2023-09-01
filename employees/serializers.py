6d81f0063d43ae145c791d71dbb78277133f490c
Mahbubur Rahman <104581346+mahbubu1640@users.noreply.github.com>
2023-09-01 15:31:20 +0530

    serializers.py

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,18 +1,16 @@
 
 from rest_framework import serializers
 from .models import Employee
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
 
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ['username', 'password']
